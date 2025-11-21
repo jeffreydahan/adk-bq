41630387e22ec2b29d3839e19811c0f94999b6fc
jeffd <jeffd@test.com>
2025-11-21 18:36:51 +0000

    dymanically works locally and in Agent Engine/Gemini Enterprise.  Need to document now.

--- adk-bq/app/agent.py
+++ adk-bq/app/agent.py
@@ -31,6 +31,7 @@ from google.adk.apps.app import App
 import os
 import sys, re, json
 import logging
+import google.cloud.logging
 
 from typing import Any, Dict, Optional
 from google.adk.agents import Agent
@@ -59,10 +60,24 @@ from .tools import app_int_cloud_bqoauth_connector
 
 load_dotenv()
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.ERROR)
+IS_RUNNING_IN_GCP = os.getenv("K_SERVICE") is not None
 
-print("Libraries imported.")
+# Check if running in a GCP environment
+if IS_RUNNING_IN_GCP:
+    # Set up Google Cloud Logging
+    client = google.cloud.logging.Client()
+    client.setup_logging()
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Running in GCP. Configured Google Cloud Logging.")
+else:
+    # Set up basic logging for local development
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Running locally. Using basic console logging.")
+
+logger = logging.getLogger(__name__)
+
+
+logger.info("Libraries imported.")
 
 project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
 
@@ -70,33 +85,63 @@ auth_id=os.getenv("BQ_AUTHORIZATION_ID")
 dynamic_auth_param_name = "dynamic_auth_config" # Name of the parameter to inject
 dynamic_auth_internal_key = "oauth2_auth_code_flow.access_token" # Internal key for the token
 
-def dynamic_token_injection(tool: BaseTool, args: Dict[str, Any], tool_context: ToolContext) -> Optional[Dict]:
+def get_local_dev_token() -> str:
+    """Function which checks if the agent is running locally via ADK web and if so,
+    obtains an access token using google-auth library and stores it in the 
+    tool context state.
+    
+    If running in the cloud, the env variable K_SERVICE will be set, and the function
+    will return without taking any action, as the token is handled automatically.
+    
+    If running locally, the K_SERVICE env variable will not be set, and the function 
+    will attempt to get the token and store it in the tool context state under the 
+    auth_id key.
+    """
     # If not running in a deployed Cloud Run environment (e.g., running locally)
-    if not os.getenv("K_SERVICE"):
+    if not IS_RUNNING_IN_GCP:
         try:
             credentials, project = google.auth.default(scopes=["https://www.googleapis.com/auth/cloud-platform"])
             request = google.auth.transport.requests.Request()
             credentials.refresh(request)
-            token = credentials.token
-            tool_context.state[auth_id] = token
-            print("Running locally, obtained token using google-auth.")
+            logger.info("Running locally in ADK, obtained token using google-auth.")
+            token_key = credentials.token
+            
         except Exception as e:
-            print(f"Could not get access token using google-auth: {e}")
+            logger.error(f"Could not get access token using google-auth: {e}")
             # Fallback to trying to find the token in the state, in case it was set by other means
             pass
     else:
-        print("Running in Agent Engine or Gemini Enterprise. OAUTH handled automatically.")
+        logger.info(f"Running on Agent Engine or Gemini Enterprise. OAUTH handled automatically.  Confirmed by finding environment variable K_SERVICE={os.getenv('K_SERVICE')}")
+        token_key = None
+    
+    return token_key
+
+
+def dynamic_token_injection(tool: BaseTool, args: Dict[str, Any], tool_context: ToolContext) -> Optional[Dict]:
+    # Call function to get local dev token if agent is running via adk web (locally)
+    token_key = None
+    # get local token key if running locally
+    token_key = get_local_dev_token()
+    
+    if token_key:
+        # running locally and obtained token
+        tool_context.state['temp:'+auth_id+'_0'] = token_key
+    
+    pattern = re.compile(r'.*'+auth_id+'.*')
+    # logger.info("Checking for pattern using regex: %s", pattern.pattern)
 
     state_dict = tool_context.state.to_dict()
-
-    if auth_id in state_dict:
-        access_token = state_dict[auth_id]
-        dynamic_auth_config = {dynamic_auth_internal_key: access_token}
-        args[dynamic_auth_param_name] = json.dumps(dynamic_auth_config)
+    # logger.info("Current tool context state keys: %s", state_dict)
+    matched_auth = {key: value for key, value in state_dict.items() if pattern.match(key)}
+    if len(matched_auth) > 0:
+        token_key = list(matched_auth.keys())[0]
     else:
-        print(f"No valid token found for key: {auth_id}")
+        logger.warning("No valid tokens found")
         return None
-    
+        
+    access_token = state_dict[token_key]
+    dynamic_auth_config = {dynamic_auth_internal_key: access_token}
+    args[dynamic_auth_param_name] = json.dumps(dynamic_auth_config)
     return None
 
 # Define the bqoauth Agent with tools and instructions
